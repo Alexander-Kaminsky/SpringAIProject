9144f741a9f0661dd0bea0dfdef2b46c8c713f20
Alexander-Kaminsky <A13xand371@gmail.com>
2025-06-30 15:04:45 +0300

    SpringAI code

---
+++ AI_SmartCareer/src/main/java/cloud/SmartCareerAIApplication.java
@@ -0,0 +1,13 @@
+package cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartCareerAIApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartCareerAIApplication.class, args);
+    }
+
+}
